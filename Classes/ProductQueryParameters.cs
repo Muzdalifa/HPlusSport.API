fe1690c8c3dfe1b4893ee598f7ee2b54eb3e0f28
Muzdalifa <a.muzdalifa27@gmail.com>
2020-06-11 15:35:29 +0200

    Filtering items

---
+++ Classes/ProductQueryParameters.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HPlusSport.API.Classes
+{
+    public class ProductQueryParameters:QueryParameters //we dont want to loose the prpoerty that we have in query paramet class
+    {
+        public string Sku { get; set; }
+        public decimal? MinPrice { get; set; }
+        public decimal? MaxPrice { get; set; }
+
+    }
+}
--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -78,9 +78,24 @@ namespace HPlusSport.API.Controllers
 
         //Getting all data by once result into problem if the data is too large, so we divide them using size and we can see other using pagination. 
         [HttpGet]
-        public async Task<IActionResult> GetAllProducts([FromQuery] QueryParameters queryParameters) //to see the amount of product you want you need to specify that amount as int here, and to see others you need to specify page number 
+        public async Task<IActionResult> GetAllProducts([FromQuery] ProductQueryParameters queryParameters) //to see the amount of product you want you need to specify that amount as int here, and to see others you need to specify page number 
         {
             IQueryable<Product> products = _context.Products;
+
+            //filtering by price
+            if (queryParameters.MinPrice != null && queryParameters.MaxPrice != null) //we check if MaxPrice and MinPrice has been set
+            {
+                products = products.Where(
+                    p => p.Price >= queryParameters.MinPrice.Value &&
+                    p.Price <= queryParameters.MaxPrice.Value);
+            }
+
+            //filtering by sku
+            if(!string.IsNullOrEmpty(queryParameters.Sku))
+            {
+                products = products.Where(p=>p.Sku == queryParameters.Sku);
+            }
+
             products = products
                 .Skip(queryParameters.Size * (queryParameters.Page - 1))
                 .Take(queryParameters.Size);
